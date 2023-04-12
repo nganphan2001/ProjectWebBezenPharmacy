afb64e318e0973b17cca7561297f06f887778174
LuongThu186 <thula20406@st.uel.edu.vn>
2023-04-12 16:50:34 +0700

    header

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignupComponent } from './signup/signup.component';
 import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -21,20 +19,18 @@ import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
 import { PaymentComponent } from './payment/payment.component';
 import { PaymentMomoComponent } from './payment-momo/payment-momo.component';
 import { PaymentBankingComponent } from './payment-banking/payment-banking.component';
-import { AdminCartManagementComponent } from './admin-cart-management/admin-cart-management.component';
-import { AdminOrdermanagementComponent } from './admin-ordermanagement/admin-ordermanagement.component';
-import { AdminOrerManagementComponent } from './admin-orer-management/admin-orer-management.component';
 import { AdminProductManagementComponent } from './admin-product-management/admin-product-management.component';
 import { AdminCaterogyManagementComponent } from './admin-caterogy-management/admin-caterogy-management.component';
 import { AdminCustonerManagementComponent } from './admin-custoner-management/admin-custoner-management.component';
+import { AdminOrderManagementComponent } from './admin-order-management/admin-order-management.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+  AppComponent,
+      AppComponent,
     HeaderComponent,
     FooterComponent,
     LoginComponent,
-    SignUpComponent,
     SignupComponent,
     ForgotpasswordComponent,
     ProfileComponent,
@@ -49,12 +45,10 @@ import { AdminCustonerManagementComponent } from './admin-custoner-management/ad
     PaymentComponent,
     PaymentMomoComponent,
     PaymentBankingComponent,
-    AdminCartManagementComponent,
-    AdminOrdermanagementComponent,
-    AdminOrerManagementComponent,
     AdminProductManagementComponent,
     AdminCaterogyManagementComponent,
-    AdminCustonerManagementComponent
+    AdminCustonerManagementComponent,
+    AdminOrderManagementComponent
   ],
   imports: [
     BrowserModule,
